874a9a16a6ca9f685bb586cd09ebff7f2aee37f6
turkimajdi <56868685+turkimajdi@users.noreply.github.com>
2022-02-27 22:51:08 +0100

    kk

--- lib/Acceuil.dart
+++ lib/Acceuil.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:projectflutter_2022/constants.dart';
 import 'screens/home/components/body.dart';
+import 'Login.dart';
 
 class Acceuil extends StatefulWidget {
   const Acceuil({Key? key}) : super(key: key);
@@ -14,38 +16,65 @@ class _AcceuilState extends State<Acceuil> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Body(),
+
+      body: Body(
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Mon compte'),
+            ),
+            ListTile(
+              title: const Text('Deconnexion'),
+              onTap: ()async {
+                Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>  Login()),
+                );
+                await FirebaseAuth.instance.signOut();
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   AppBar buildAppBar() {
+
     return AppBar(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      leading: IconButton(
-        icon: SvgPicture.asset("assets/icons/back.svg"),
-        onPressed: () {},
-      ),
+      backgroundColor: Color(0xFF0098c1),
+      title: Text("Shop"),
+      automaticallyImplyLeading: true,
+
       actions: <Widget>[
-        IconButton(
-          icon: SvgPicture.asset(
-            "assets/icons/search.svg",
-            // By default our  icon color is white
-            color: kTextColor,
-          ),
-          onPressed: () {},
-        ),
+
+
+
         IconButton(
           icon: SvgPicture.asset(
             "assets/icons/cart.svg",
             // By default our  icon color is white
             color: kTextColor,
+
           ),
           onPressed: () {},
         ),
+
         SizedBox(width: kDefaultPaddin / 2)
       ],
     );
+
   }
+
 
 }
